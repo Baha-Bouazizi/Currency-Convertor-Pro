4df806003727b6384202dcb688f85996cc21d8a8
Bouazizi Baha <166825233+Baha-Bouazizi@users.noreply.github.com>
2024-08-05 17:04:21 +0100

    update

--- Js/LogiqueDeConversion/CurrencyConverter.js
+++ Js/LogiqueDeConversion/CurrencyConverter.js
@@ -8,6 +8,31 @@ class CurrencyConverter {
         this.apiKeyAlphaVantage = '6A87GQBCVBN5HPW6'; // ll graphe wl wekly chnages
     }
 
+    checkForRateChanges(newRates) {
+        const fromCurrency = document.querySelector("#from").value.toUpperCase();
+        const toCurrency = document.querySelector("#to").value.toUpperCase();
+
+        // Simulate random change for demo purposes     devise to
+        const randomChangeto = (Math.random() * 0.1 - 0.05) * this.previousRates[toCurrency]; // Between -5% and +5%
+        newRates[toCurrency] += randomChangeto;
+
+        if (this.previousRates[toCurrency]) {
+            const change = ((newRates[toCurrency] - this.previousRates[toCurrency]) / this.previousRates[toCurrency]) * 100;
+            if (Math.abs(change) > 1) {
+                this.showNotification(`Exchange rate for ${toCurrency} changed by ${change.toFixed(2)}%`, change > 0 ? 'success' : 'error');
+            }
+        }
+        const randomChangefrom = (Math.random() * 0.6 - 0.05) * this.previousRates[fromCurrency]; // Between -5% and +5%
+        newRates[fromCurrency] += randomChangefrom;
+
+        if (this.previousRates[fromCurrency]) {
+            const change = ((newRates[fromCurrency] - this.previousRates[fromCurrency]) / this.previousRates[fromCurrency]) * 100;
+            if (Math.abs(change) > 1) {
+                this.showNotification(`Exchange rate for ${from} changed by ${change.toFixed(2)}%`, change > 0 ? 'success' : 'error');
+            }
+        }
+    }   
+
     async fetchRates() {
         try {
             const url = 'https://open.er-api.com/v6/latest/USD'; // Reliable API
@@ -29,43 +54,57 @@ class CurrencyConverter {
         }
     }
 
-    async fetchHistoricalData(baseCurrency, targetCurrency) {
+    async fetchHistoricalData(baseCurrency, targetCurrency, period) {
         const url = `https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=${baseCurrency}&to_symbol=${targetCurrency}&apikey=${this.apiKeyAlphaVantage}`;
-    
+        
         try {
             const res = await fetch(url);
             const data = await res.json();
-    
-            // Vérifiez la structure des données
+        
             console.log('Fetched data:', data);
-    
+        
             if (data["Time Series FX (Daily)"]) {
-                const historicalData = Object.keys(data["Time Series FX (Daily)"]).slice(0, 359).map(date => ({
+                const historicalData = Object.keys(data["Time Series FX (Daily)"]).map(date => ({
                     date,
                     rate: parseFloat(data["Time Series FX (Daily)"][date]["4. close"]),
                     min: parseFloat(data["Time Series FX (Daily)"][date]["3. low"]),
                     max: parseFloat(data["Time Series FX (Daily)"][date]["2. high"]),
                 }));
     
-                // Vérifiez les données historiques
-                console.log('Historical Data:', historicalData);
+                // Calculer le nombre de jours à afficher en fonction de la période
+                let daysToShow;
+                switch (period) {
+                    case '1w': // 1 Semaine
+                        daysToShow = 7;
+                        break;
+                    case '1m': // 1 Mois
+                        daysToShow = 30;
+                        break;
+                    case '1y': // 1 An
+                        daysToShow = 365;
+                        break;
+                   
+                  
+                    default:
+                        daysToShow = 7;
+                }
     
-                // Get data for the last 7 days
-                const lastWeekData = historicalData.slice(0, 7);
-                console.log('Last Week Data:', lastWeekData);
+                // Filtrer les données pour la période sélectionnée
+                const periodData = historicalData.slice(0, daysToShow);
+                console.log(`${period} Data:`, periodData);
     
-                const minRate = Math.min(...lastWeekData.map(data => data.min));
-                const maxRate = Math.max(...lastWeekData.map(data => data.max));
-                const lastCloseRate = lastWeekData[0].rate;
-    
-                return { historicalData, minRate, maxRate, lastCloseRate, lastWeekData };
+                const minRate = Math.min(...periodData.map(data => data.min));
+                const maxRate = Math.max(...periodData.map(data => data.max));
+                const lastCloseRate = periodData[0] ? periodData[0].rate : 0;
+        
+                return { historicalData: periodData, minRate, maxRate, lastCloseRate };
             } else {
                 console.error('No historical data found or API error:', data);
             }
         } catch (error) {
             console.error('Error fetching historical data:', error.message);
         }
-        return { historicalData: [], minRate: 0, maxRate: 0, lastCloseRate: 0, lastWeekData: [] };
+        return { historicalData: [], minRate: 0, maxRate: 0, lastCloseRate: 0 };
     }
     
     
@@ -101,6 +140,7 @@ class CurrencyConverter {
         }
         const convertedValue = (amount / fromRate) * toRate;
         return convertedValue; 
+     
     }
     
     
@@ -141,19 +181,20 @@ class CurrencyConverter {
     async updateChart() {
         const fromCurrency = document.querySelector("#from").value.toUpperCase();
         const toCurrency = document.querySelector("#to").value.toUpperCase();
-        const formattedResult = ` ${fromCurrency.toUpperCase()}  To  ${toCurrency.toUpperCase()}`;
-
+        const period = document.querySelector("#period").value; // Récupérer la période sélectionnée
+        const formattedResult = ` ${fromCurrency.toUpperCase()} To ${toCurrency.toUpperCase()} (${period.toUpperCase()})`;
     
         try {
-            const { historicalData, minRate, maxRate, lastCloseRate } = await this.fetchHistoricalData(fromCurrency, toCurrency);
+            // Récupérer les données historiques pour la période sélectionnée
+            const { historicalData, minRate, maxRate, lastCloseRate } = await this.fetchHistoricalData(fromCurrency, toCurrency, period);
     
             const ctx = document.getElementById('exchangeRateChart').getContext('2d');
     
             if (this.chart) {
-                this.chart.destroy(); // Destroy the previous chart instance
+                this.chart.destroy(); // Détruire l'instance précédente du graphique
             }
     
-            // Create the chart
+            // Créer le graphique
             this.chart = new Chart(ctx, {
                 type: 'line',
                 data: {
@@ -162,20 +203,20 @@ class CurrencyConverter {
                         {
                             label: `${fromCurrency} to ${toCurrency}`,
                             data: historicalData.map(data => data.rate),
-                            borderColor: '#2ECC71', // Green Xe
-                            backgroundColor: 'rgba(46, 204, 113, 0.2)', // Slightly transparent green background
+                            borderColor: '#2ECC71', // Vert pour le taux de change
+                            backgroundColor: 'rgba(46, 204, 113, 0.2)', // Arrière-plan vert légèrement transparent
                             borderWidth: 2,
-                            pointBackgroundColor: '#2ECC71', // Green Xe for points
-                            pointBorderColor: '#fff', // White for point borders
-                            pointHoverBackgroundColor: '#fff', // White background on hover
-                            pointHoverBorderColor: '#2ECC71', // Green Xe border on hover
+                            pointBackgroundColor: '#2ECC71', // Vert pour les points
+                            pointBorderColor: '#fff', // Blanc pour les bordures des points
+                            pointHoverBackgroundColor: '#fff', // Blanc pour le fond des points au survol
+                            pointHoverBorderColor: '#2ECC71', // Vert pour les bordures des points au survol
                             tension: 0.4
                         },
                         {
                             label: `${fromCurrency} to ${toCurrency} (Min)`,
                             data: historicalData.map(data => data.min),
-                            borderColor: '#FF5733', // Red for min rate
-                            backgroundColor: 'rgba(255, 87, 51, 0.2)', // Slightly transparent red background
+                            borderColor: '#FF5733', // Rouge pour le taux minimum
+                            backgroundColor: 'rgba(255, 87, 51, 0.2)', // Arrière-plan rouge légèrement transparent
                             borderWidth: 1,
                             pointBackgroundColor: '#FF5733',
                             pointBorderColor: '#fff',
@@ -186,8 +227,8 @@ class CurrencyConverter {
                         {
                             label: `${fromCurrency} to ${toCurrency} (Max)`,
                             data: historicalData.map(data => data.max),
-                            borderColor: '#3498DB', // Blue for max rate
-                            backgroundColor: 'rgba(52, 152, 219, 0.2)', // Slightly transparent blue background
+                            borderColor: '#3498DB', // Bleu pour le taux maximum
+                            backgroundColor: 'rgba(52, 152, 219, 0.2)', // Arrière-plan bleu légèrement transparent
                             borderWidth: 1,
                             pointBackgroundColor: '#3498DB',
                             pointBorderColor: '#fff',
@@ -222,13 +263,13 @@ class CurrencyConverter {
                             beginAtZero: false,
                             title: {
                                 display: true,
-                                text: 'Exchange Rate',
+                                text: 'Taux de Change',
                                 font: {
                                     size: 16
                                 }
                             },
                             grid: {
-                                color: 'rgba(200, 200, 200, 0.3)' // Light gray for grid
+                                color: 'rgba(200, 200, 200, 0.3)' // Gris clair pour la grille
                             }
                         },
                         x: {
@@ -240,31 +281,31 @@ class CurrencyConverter {
                                 }
                             },
                             grid: {
-                                color: 'rgba(200, 200, 200, 0.3)' // Light gray for grid
+                                color: 'rgba(200, 200, 200, 0.3)' // Gris clair pour la grille
                             }
                         }
                     }
                 }
             });
-
-            // Add background text
+    
+            // Ajouter du texte de fond
             const originalDraw = this.chart.draw;
             this.chart.draw = function() {
                 originalDraw.apply(this, arguments);
     
                 ctx.save();
                 ctx.font = 'bold 24px Arial';
-                ctx.fillStyle = 'rgba(46, 204, 113, 0.3)'; // Slightly transparent green Xe
+                ctx.fillStyle = 'rgba(46, 204, 113, 0.3)'; // Vert légèrement transparent
                 ctx.textAlign = 'center';
-                ctx.fillText('Currency Charts'+' : '+formattedResult, this.width / 2, this.height / 2);
+                ctx.fillText(`Currency Charts  : ${formattedResult}`, this.width / 2, this.height / 2);
                 ctx.restore();
             };
     
         } catch (error) {
-            console.error('Error updating the chart:', error.message);
+            console.error('Erreur lors de la mise à jour du graphique:', error.message);
         }
     }
-
+    
     
     ////////////////////////////////////////////
 
@@ -387,17 +428,18 @@ class CurrencyConverter {
         const toCurrency = document.querySelector("#to");
         const inputAmount = document.querySelector(".input-amount");
         const swapBtn = document.querySelector(".swap-btn");
-
         fromCurrency.addEventListener("change", () => {
             this.convertAndDisplay();
             this.updateChart();
             this.updateFlagIcons(); // Update flags
+            this.checkForRateChanges();
 this.scrollToChartSection        });
 
         toCurrency.addEventListener("change", () => {
             this.convertAndDisplay();
             this.updateChart();
-            this.updateFlagIcons(); // Update flags
+            this.updateFlagIcons(); // Update flagsch
+            this.checkForRateChanges();
 
         });
 
@@ -413,6 +455,7 @@ this.scrollToChartSection        });
             this.updateFlagIcons(); // Update flags
 
         });
+     
     }
 
     isUserLoggedIn() {
@@ -470,7 +513,6 @@ this.scrollToChartSection        });
         history.push({ date: new Date(), amount, from, to, converted });
         localStorage.setItem(userHistoryKey, JSON.stringify(history));
     }
-
     showNotification(message, type) {
         const notificationContainer = document.getElementById('notification-container');
         const notification = document.createElement('div');
@@ -482,18 +524,21 @@ this.scrollToChartSection        });
             notification.remove();
         }, 5000);
     }
+    
 
-    async initialize() {
+    ;async initialize() {
         await this.fetchRates();
         this.populateSelects();
         this.addEventListeners();
         this.updateChart();
         this.fetchPopularPairs() ;
         this.calculateWeeklyChange() ;
+        this.showNotification();
         setInterval(() => {
             this.fetchRates();
+          
           // Mettez à jour les taux d'intérêt
-        }, 300000); // Check for rate changes every 5 minutes
+        }, 300000) // Check for rate changes every 5 minutes
 
         // Display profile link if user is logged in
         const user = JSON.parse(localStorage.getItem('currentUser'));
@@ -506,6 +551,10 @@ this.scrollToChartSection        });
                 // Cache le lien du profil si l'utilisateur n'est pas connecté
                 profileLink.style.display = 'none';
             }
+        });
+        document.getElementById('period').addEventListener('change', function() {
+            const period = this.value;
+            fetchHistoricalData(period);
         });
     }
 }
