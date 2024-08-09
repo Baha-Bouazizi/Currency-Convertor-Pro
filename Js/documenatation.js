04337b70718dc887ec880b4f79cf7c80dff3ce71
Bouazizi Baha <166825233+Baha-Bouazizi@users.noreply.github.com>
2024-08-09 11:38:37 +0100

    delete documentations files

--- Js/documenatation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const documentationBtn = document.getElementById('documentation-btn');
-
-    if (documentationBtn) {
-        documentationBtn.addEventListener('click', () => {
-            window.location.href = 'documentation.html'; // Remplacez par l'URL de votre page de documentation
-        });
-    }
-});
